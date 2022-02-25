3c382a23b5e1ab2c5ff0bb9641556bdaeec641c0
Aditya Kulkarni <thecoreprogrammer@gmail.com>
2022-02-25 18:30:57 +0530

    Validate args

--- main.go
+++ main.go
@@ -18,22 +18,23 @@ var errInvalidArguments = errors.New("Invalid arguments")
 var errReadingInput = errors.New("Error reading input")
 
 func main() {
-
 	if(len(os.Args) != 2){
-		printError(errors.New("errInvalidArguments"))
-	}else{
-		originUnit = strings.ToUpper(os.Args[1])
-		for {
-			fmt.Print("What is the current temperature in " + originUnit + " ? ")
-			
-			fmt.Print("Would you like to convert another temperature ? (y/n) ")
-			
-			if shouldConvertAgain != "Y" {
-				fmt.Println("Good bye!")
-				break
-			}
+		printError(errInvalidArguments)
+	}
+
+	originUnit = strings.ToUpper(os.Args[1])
+	
+	for {
+		fmt.Print("What is the current temperature in " + originUnit + " ? ")
+		
+		fmt.Print("Would you like to convert another temperature ? (y/n) ")
+		
+		if shouldConvertAgain != "Y" {
+			fmt.Println("Good bye!")
+			break
 		}
 	}
+	
 }
 
 func printError(err error) {
